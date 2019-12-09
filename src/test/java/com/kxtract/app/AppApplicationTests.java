8a3ce6b866f6c99f951e271445bfd303b456039b
Frank Kelly <kellyfj@Franks-Home-MacBook-Pro.local>
2019-12-09 18:39:53 -0500

    .gitignore

---
+++ src/test/java/com/kxtract/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kxtract.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
