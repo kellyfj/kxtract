47c619e68a733b3818c686343b81952582499f8d
Frank Kelly <kellyfj@gmail.com>
2019-12-12 14:02:41 -0500

    Add more S3 tests

--- src/main/java/com/kxtract/s3/S3Uploader.java
+++ src/main/java/com/kxtract/s3/S3Uploader.java
@@ -5,6 +5,7 @@ import java.io.File;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
 public class S3Uploader {
@@ -20,10 +21,39 @@ public class S3Uploader {
     }
 	
 	public static void uploadFileToS3(String bucketName, File f) {
+		if (bucketName == null) {
+			throw new IllegalArgumentException("bucketName cannot be null");
+		}
+		if (f == null) {
+			throw new IllegalArgumentException("File cannot be null");
+		}
+		if (!f.exists()) {
+			throw new IllegalArgumentException("File (" + f.getAbsolutePath() + ") does not exist");
+		}
 		AmazonS3 s3 =  AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_1).build();   
 		PutObjectRequest p = new PutObjectRequest(bucketName, f.getName(), f);
 		
 		// Put Object
 		s3.putObject(p);
 	}
+
+	/**
+	 * For Testing only
+	 */
+	static void deleteFileFromS3(String bucketName, String fileName) {
+		AmazonS3 s3 =  AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_1).build();      
+        try {
+            boolean exists = s3.doesObjectExist(bucketName, fileName);
+			if (!exists) {
+				throw new IllegalArgumentException(
+						"That file (" + fileName + ") does not exist in S3 bucket (" + bucketName + ")");
+			}
+			DeleteObjectRequest d = new DeleteObjectRequest(bucketName, fileName);
+			s3.deleteObject(bucketName, fileName);
+        } catch (Exception e) {
+        	throw new RuntimeException(e);
+        }
+	}
 }
+
+
--- src/test/java/com/kxtract/s3/S3UploaderTest.java
+++ src/test/java/com/kxtract/s3/S3UploaderTest.java
@@ -2,6 +2,11 @@ package com.kxtract.s3;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import org.junit.jupiter.api.Test;
 
@@ -33,4 +38,48 @@ public class S3UploaderTest {
 		
 		assertFalse(S3Uploader.fileAlreadyExistsInS3(bucketName, fileName));		
 	}
+	
+	@Test
+	public void testUploadFail_NullFile() {	
+		String bucketName = null;
+		File file = null;
+		assertThrows(IllegalArgumentException.class,
+				()->{S3Uploader.uploadFileToS3(bucketName, file);} );
+	}
+	
+	@Test
+	public void testUploadFail_NullBucket() {	
+		String bucketName = null;
+		File file = new File("/tmp/test.txt");
+		assertThrows(IllegalArgumentException.class,
+				()->{S3Uploader.uploadFileToS3(bucketName, file);} );
+	}
+		
+	@Test
+	public void testUploadFail_FileDoesNotExist() {	
+		String bucketName = "kxtract";
+		File file = new File("/tmp/test.txt");
+		assertThrows(IllegalArgumentException.class,
+				()->{S3Uploader.uploadFileToS3(bucketName, file);} );
+	}
+
+	@Test
+	public void testUploadOK() throws URISyntaxException {			
+		File resourcesDirectory = new File("src/test/resources");
+		File file = new File(resourcesDirectory.getAbsolutePath() + "/testpodcasts.txt");
+		String fileName = file.getName();
+		String bucketName = "kxtract";
+		
+		//IF file already present THEN delete it
+		if(S3Uploader.fileAlreadyExistsInS3(bucketName, fileName)) {
+			S3Uploader.deleteFileFromS3(bucketName, fileName);
+		}
+		S3Uploader.uploadFileToS3(bucketName, file);
+		
+		assertTrue(S3Uploader.fileAlreadyExistsInS3(bucketName, fileName));
+		
+		//Cleanup after myself
+		S3Uploader.deleteFileFromS3(bucketName, fileName);
+	}
 }
+
