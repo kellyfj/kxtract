272a64bfba6223d3cb06e3a6edc1a5769600ba5f
Frank Kelly <kellyfj@gmail.com>
2020-01-02 15:55:32 -0500

    Add /episodes support

--- src/main/java/com/kxtract/controller/PodcastController.java
+++ src/main/java/com/kxtract/controller/PodcastController.java
@@ -5,9 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kxtract.data.EpisodeRepository;
 import com.kxtract.data.PodcastRepository;
+import com.kxtract.data.dao.Episode;
 import com.kxtract.data.dao.Podcast;
 
 @RestController
@@ -16,9 +19,22 @@ public class PodcastController {
 	@Autowired
 	private PodcastRepository podRepo;
 	
+	@Autowired
+	private EpisodeRepository episodeRepo;
+	
 	@GetMapping("/api/podcasts")
 	public List<Podcast> podcasts(Model model) {
 		return (List<Podcast>) podRepo.findAll();
+	}
+
+	@GetMapping("/api/episodes")
+	public List<Episode> episodes(Model model) {
+		return (List<Episode>) episodeRepo.findAll();
+	}
+	
+	@GetMapping("/api/podcast/{podcastId}/episodes")
+	public List<Episode> episodes(@PathVariable int podcastId, Model model) {
+		return (List<Episode>) episodeRepo.findByPodcastId(podcastId);
 	}
 
 }
--- src/main/java/com/kxtract/controller/PodcastUIController.java
+++ src/main/java/com/kxtract/controller/PodcastUIController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.kxtract.data.EpisodeRepository;
 import com.kxtract.data.PodcastRepository;
+import com.kxtract.data.dao.Episode;
 import com.kxtract.data.dao.Podcast;
 
 @Controller
@@ -15,6 +18,9 @@ public class PodcastUIController {
 
 	@Autowired
 	private PodcastRepository podRepo;
+	
+	@Autowired
+	private EpisodeRepository episodeRepo;
 	
 	@GetMapping("/")
 	public String index(Model model) {
@@ -27,6 +33,23 @@ public class PodcastUIController {
 		List<Podcast> podcastList = (List<Podcast>) podRepo.findAll();
 		model.addAttribute("list", podcastList);
 		return "podcasts";
+	}
+	
+	@GetMapping("/ui/episodes")
+	public String episodes(Model model) {
+		List<Episode> episodeList = (List<Episode>) episodeRepo.findAll();
+		model.addAttribute("podcastId", "ALL");
+		model.addAttribute("episodeList", episodeList);
+		return "episodes";
+	}
+	
+	@GetMapping("/ui/podcast/{podcastId}/episodes")
+	public String episodes(@PathVariable int podcastId, Model model) {
+		Podcast p = podRepo.findById(podcastId);
+		List<Episode> episodeList = (List<Episode>) episodeRepo.findByPodcastId(podcastId);
+		model.addAttribute("podcastId", p.getName());
+		model.addAttribute("episodeList", episodeList);
+		return "episodes";
 	}
 
 }
