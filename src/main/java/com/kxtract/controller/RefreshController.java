73aa574bc6765bc3b844a00396b78a1695f6f63e
Frank Kelly <kellyfj@gmail.com>
2020-01-14 16:50:55 -0500

    Support thread per downloader for faster refresh

--- src/main/java/com/kxtract/controller/RefreshController.java
+++ src/main/java/com/kxtract/controller/RefreshController.java
@@ -5,6 +5,11 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.tomcat.util.http.fileupload.FileUtils;
@@ -65,9 +70,69 @@ public class RefreshController {
 		List<com.kxtract.data.dao.Episode> episodesUploadedToS3 = new ArrayList<>();
 		List<String> stackTraces = new ArrayList<>();
 		List<Podcast> podcastList = (List<Podcast>) podRepo.findAll();
-		int podcastCount = 0;
-		for (Podcast p : podcastList) {
+		
+		if (podcastList.size() > 0) {
+			// Parallelize Podcast Downloading via Thread pool
+			int threadPoolSize = Math.min(20, podcastList.size());
+			logger.info("With " + podcastList.size() + " podcasts - creating a pool of size " + threadPoolSize
+					+ " threads . . . ");
+			ExecutorService executorService = Executors.newFixedThreadPool(threadPoolSize);
+			int podcastCount = 0;
 
+			List<Future<TaskResult>> futureList = new ArrayList<>();
+			for (Podcast p : podcastList) {
+				Future<TaskResult> future = executorService.submit(new PodcastDownloaderTask(p));
+				futureList.add(future);
+				podcastCount++;
+				logger.info("Processed Podcast " + podcastCount + " of " + podcastList.size() + "");
+			}
+			// Collect results
+			try {
+				for (Future<TaskResult> f : futureList) {
+					TaskResult t = f.get();
+					if (t.episodeDownloaded != null) {
+						episodesDownloaded.add(t.episodeDownloaded);
+					}
+					if (t.episodeUploaded != null) {
+						episodesUploadedToS3.add(t.episodeUploaded);
+					}
+					if (t.stackTraceIfAny != null) {
+						stackTraces.add(t.stackTraceIfAny);
+					}
+				}
+			} catch (ExecutionException ee) {
+				logger.error(ee.getMessage(), ee);
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			} finally {
+				if (executorService != null && !executorService.isShutdown()) {
+					executorService.shutdown();
+				}
+			}
+		}
+		logger.info("REFRESH COMPLETE!");
+		model.addAttribute("episodeDownloads", episodesDownloaded);
+		model.addAttribute("episodesUploadedToS3", episodesUploadedToS3);
+		model.addAttribute("errorStackTraces", stackTraces);
+		return "refresh";
+	}
+	
+	private class TaskResult {
+		public com.kxtract.data.dao.Episode episodeDownloaded;
+		public com.kxtract.data.dao.Episode episodeUploaded;
+		public String stackTraceIfAny;
+	}
+	
+	class PodcastDownloaderTask implements Callable<TaskResult> {
+		Podcast p;
+		
+		public PodcastDownloaderTask(Podcast podcast) {
+			this.p = podcast;
+		}
+		
+		@Override
+		public TaskResult call() throws Exception {
+			TaskResult result = new TaskResult();
 			try {
 				Episode ep = PodcastDownloader.downloadLatestEpisode(p.getRssURL(), DOWNLOAD_PATH, false);
 
@@ -80,7 +145,7 @@ public class RefreshController {
 						com.kxtract.data.dao.Episode newEpisode = new com.kxtract.data.dao.Episode(p.getId(),
 								ep.getTitle(), null, originURL.toString(), null, ep.getFileSizeInKB());
 
-						episodesDownloaded.add(newEpisode);
+						result.episodeDownloaded = newEpisode;
 						logger.info("Checking S3 before upload . . . .");
 						String episodeFilename = ep.getFilename();
 						URL s3URL = null;
@@ -89,17 +154,19 @@ public class RefreshController {
 							s3URL = S3Uploader.getObjectURL(RAW_AUDIO_BUCKET_NAME, episodeFilename);
 						} else {
 							logger.info("Uploading File (" + episodeFilename + ") to S3 . . . ");
-							s3URL = S3Uploader.uploadFileToS3(RAW_AUDIO_BUCKET_NAME, new File(DOWNLOAD_PATH + episodeFilename));
+							s3URL = S3Uploader.uploadFileToS3(RAW_AUDIO_BUCKET_NAME,
+									new File(DOWNLOAD_PATH + episodeFilename));
 							logger.info("File (" + episodeFilename + ") Upload complete!");
-							episodesUploadedToS3.add(newEpisode);
-							
-							//String jobName = Transcriber.launchTranscriptionJob(RAW_AUDIO_BUCKET_NAME, episodeFilename,
-							//	TRANSCRIPTION_BUCKET_NAME);
-							//logger.info("Transcription Job Started --> " + jobName);
+							result.episodeUploaded = newEpisode;
+
+							// String jobName = Transcriber.launchTranscriptionJob(RAW_AUDIO_BUCKET_NAME,
+							// episodeFilename,
+							// TRANSCRIPTION_BUCKET_NAME);
+							// logger.info("Transcription Job Started --> " + jobName);
 						}
 						newEpisode.setProcessingStatus(ProcessingStatus.DOWNLOADED);
 						newEpisode.setS3URL(s3URL.toString());
-						episodeRepo.save(newEpisode);				
+						episodeRepo.save(newEpisode);
 					} else {
 						logger.warn("Episode ( " + ep.getTitle() + ") was already in the database");
 					}
@@ -107,15 +174,10 @@ public class RefreshController {
 			} catch (Exception e) {
 				logger.error("Exception " + e.getMessage(), e);
 				String stackTrace = ExceptionUtils.getStackTrace(e);
-				stackTraces.add(stackTrace);
+				result.stackTraceIfAny = stackTrace;
 			}
-			podcastCount++;
-			logger.info("Processed Podcast " + podcastCount + " of " + podcastList.size() + "");
+			return result;
 		}
-		logger.info("REFRESH COMPLETE!");
-		model.addAttribute("episodeDownloads", episodesDownloaded);
-		model.addAttribute("episodesUploadedToS3", episodesUploadedToS3);
-		model.addAttribute("errorStackTraces", stackTraces);
-		return "refresh";
+		
 	}
 }
--- src/main/resources/db/migration/V5__add_initial_subscriptions.sql
+++ src/main/resources/db/migration/V5__add_initial_subscriptions.sql
@@ -5,6 +5,5 @@ INSERT INTO subscription(user_id, podcast_id) VALUES ('kellyfj@gmail.com',4);
 
 INSERT INTO subscription(user_id, podcast_id) VALUES ('frank@frankkelly.us',2);
 INSERT INTO subscription(user_id, podcast_id) VALUES ('frank@frankkelly.us',5);
-INSERT INTO subscription(user_id, podcast_id) VALUES ('frank@frankkelly.us',6);
 INSERT INTO subscription(user_id, podcast_id) VALUES ('frank@frankkelly.us',7);
 
